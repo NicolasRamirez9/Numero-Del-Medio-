4af6e2a97a3e760a1f4c6bd060a5608eafafc46a
Nicolas <nicocai@gmail.com>
2018-08-27 12:25:36 -0300

    Termine el ejercicio.

--- ItineracionesUTN/main.c
+++ ItineracionesUTN/main.c
@@ -3,6 +3,38 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int numeroUno;
+    int numeroDos;
+    int numeroTres;
+
+   printf("Ingrese el primer numero: ");
+   scanf("%d", &numeroUno);
+
+   printf("Ingrese el segundo numero: ");
+   scanf("%d", &numeroDos);
+
+   printf("Ingrese el tercer numero: ");
+   scanf("%d", &numeroDos);
+
+
+    if(numeroUno > numeroDos && numeroUno < numeroTres)
+    {
+        printf("El numero del medio es el numero: %d", numeroUno);
+    }
+    else
+    {
+        if (numeroDos > numeroTres && numeroDos < numeroTres)
+        {
+            printf("El numero del medio es el numero: %d", numeroDos);
+        }
+        else
+        {
+            printf("El numero del medio es el numero: %d", numeroTres);
+        }
+    }
+    else
+    {
+        printf("El numero del medio no existe.");
+    }
+
 }
